8733dddea5229e1a6a77cce7745b9b0e25533e48
Daria_Ketova <dariaket@mail.ru>
2023-03-28 22:47:43 +0300

    features

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ var app = new Vue({
     el: '#app',
     data: {
       product: "Socks",
-      description: "A pair of warm, fuzzy socks"
+      description: "A pair of warm, fuzzy socks",
+      image: "./assets/vmSocks-green.jpg",
+      altText: "A pair of socks",
+      link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green', 
+          variantImage: "./assets/vmSocks-green.jpg"
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: "./assets/vmSocks-blue.jpg"
+        } 
+      ],
+      cart: 0
+    },
+      methods: {
+        addToCart() {
+          this.cart += 1
+        },
+
+       
+        updateProduct(variantImage) {
+          this.image = variantImage
+        }
     }
 })
